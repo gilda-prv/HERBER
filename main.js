05cc512e181efdb0260f77c8bfc615fcf2733c5f
gilda-prv <gildaaparvari@gmail.com>
2024-01-11 19:17:47 +0330

    new

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // search-box
+  $(".search-btn").click(function () {
+    $(".wrapper").addClass("active");
+    $(this).css("display", "none");
+    $(".search-data").fadeIn(500);
+    $(".close-btn").fadeIn(500);
+    $(".search-data .line").addClass("active");
+    setTimeout(function () {
+      $("input").focus();
+      $(".search-data label").fadeIn(500);
+      $(".search-data span").fadeIn(500);
+    }, 800);
+  });
+  $(".close-btn").click(function () {
+    $(".wrapper").removeClass("active");
+    $(".search-btn").fadeIn(800);
+    $(".search-data").fadeOut(500);
+    $(".close-btn").fadeOut(500);
+    $(".search-data .line").removeClass("active");
+    $("input").val("");
+    $(".search-data label").fadeOut(500);
+    $(".search-data span").fadeOut(500);
+  });
+  //carousel
+  // $(".carousel").mouseenter(function () {
+  //   setTimeout(function () {
+  //     $(".carousel-item").fadeToggle("slow");
+  //   }, 2000);
+  // });
+});
